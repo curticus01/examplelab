017533fd4ad9212e25060078e8e883ef12378ee3
Curtis Coughenour <curtis.coughenour@comcast.net>
2016-02-01 20:45:57 -0500

    My work from the example lab!

--- src/main/java/edu/carrollcc/cis232/App.java
+++ src/main/java/edu/carrollcc/cis232/App.java
@@ -1,13 +1,20 @@
 package edu.carrollcc.cis232;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println("Hello world!");
-    }
+public class App {
+	public static void main(String[] args) {
+		String name;
+		Scanner keyboard = new Scanner(System.in);
+
+		System.out.print("What is your name? ");
+		name = keyboard.nextLine();
+
+		System.out.printf("Hello %s!%n", name);
+		keyboard.close();
+	}
 }
